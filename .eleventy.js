c4a95f35af9628f659e6762d5ab494fb0565d7f9
Raylen <81475574+ausername-1@users.noreply.github.com>
2021-05-24 21:20:06 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,28 @@ module.exports = function(eleventyConfig) {
     return array.slice(0, n);
   });
   
+    eleventyConfig.addCollection("tagList", collection => {
+    const tagsObject = {}
+    collection.getAll().forEach(item => {
+      if (!item.data.tags) return;
+      item.data.tags
+        .filter(tag => !['post', 'all'].includes(tag))
+        .forEach(tag => {
+          if(typeof tagsObject[tag] === 'undefined') {
+            tagsObject[tag] = 1
+          } else {
+            tagsObject[tag] += 1
+          }
+        });
+    });
+
+    const tagList = []
+    Object.keys(tagsObject).forEach(tag => {
+      tagList.push({ tagName: tag, tagCount: tagsObject[tag] })
+    })
+    return tagList.sort((a, b) => b.tagCount - a.tagCount)
+
+  });
 
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
